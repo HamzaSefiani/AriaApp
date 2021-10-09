894413cad8752fcd9b90a75eaa219b51207cb964
HamzaSefiani <hamzasefiani9@gmail.com>
2021-10-09 12:03:13 +0000

    Updated title

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Ads from 'vue-google-adsense'
-Vue.use(require('vue-script2'))
-Vue.use(Ads.Adsense)
+
 createApp(App).mount('#app')
